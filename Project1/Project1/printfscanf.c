6560cd0b86daef2726e4da111c235a385ec84100
Ram <parkeryangga@gmail.com>
2024-01-01 11:44:02 +0900

    commit

---
+++ Project1/Project1/printfscanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main( void ) {
+
+	int age = 12;
+
+	printf("%d", age);
+
+	age = 13;
+
+	printf("%d\n", age);
+
+	double d = 15.5;
+	printf("%.1lf", d);
+
+
+
+	return 0;
+}
