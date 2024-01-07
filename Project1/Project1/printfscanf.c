b137b08484561e83bc346f5e1ddf191572e63992
Ram <parkeryangga@gmail.com>
2024-01-07 20:34:51 +0900

    c 언어 저장

--- Project1/Project1/printfscanf.c
+++ Project1/Project1/printfscanf.c
@@ -1,6 +1,6 @@
 ﻿#include <stdio.h> 
 
-int main( void ) {
+int main(void) {
 
 	int add = 3 + 7;
 
@@ -8,15 +8,15 @@ int main( void ) {
 
 	printf("변수 사용안함  3 + 7 = %d\n", add);
 
-	
-	printf("%d\n",  30 % 70); 
+
+	printf("%d\n", 30 % 70);
 
 
 	int num;
 	printf("값을 입력하세요  : ");
 	scanf_s("%d", &num);
-	printf("입력 받은 값은 %d 입니다 \n", num );
-	
+	printf("입력 받은 값은 %d 입니다 \n", num);
+
 
 	int one, two;
 	printf("두 개의 정수를 입력 하세요 : ");
@@ -26,13 +26,25 @@ int main( void ) {
 	// 문자 및 문자열
 
 	char c = 'A';
-	printf("%c\n", c) ;
+	printf("%c\n", c);
 
 	char str[256];  // 문자열 지정 
 	scanf_s("%s", str, sizeof(str));   // 문자열은 입력 받을 변수 앞에 & 생략,  크기 명시 해줘야 함 
 	fgets(str, sizeof(str), stdin);
 	printf("입력 받은 문자열은 : %s\n", str);
-	
-	
-	return 0;
+
+
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	num1 = num2 = num3 = num4 = 5;
+
+
+	printf("d c b a 차례로 값은? %d %d %d %d \n", num1, num2, num3, num4);
+
+
+
+return 0;
 }
---
+++ Project2/Project2/printfScanf2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int numA = 5;
+
+	numA += 15;
+
+	printf("numA = 5 + 15 = %d", numA);
+
+
+	return 0;
+}
