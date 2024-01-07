0147c3e29fcf0d2b4d19b2fb03e2f7b25f953da7
Ram <parkeryangga@gmail.com>
2024-01-07 20:53:30 +0900

    비트 연산자 부터 하면 됨.

--- Project2/Project2/printfScanf2.c
+++ Project2/Project2/printfScanf2.c
@@ -6,8 +6,13 @@ int main(void) {
 
 	numA += 15;
 
-	printf("numA = 5 + 15 = %d", numA);
+	printf("numA = 5 + 15 = %d \n", numA);
 
+	printf("numA++ = %d \n", numA++);
+
+	printf("++numA = %d \n", ++numA);
+
+	printf("numA = %d  여기가 최종 값 ", numA);
 
 	return 0;
 }
