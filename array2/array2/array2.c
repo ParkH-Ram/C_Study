f385faa7fb3c05867f333b4d5b42a72dd493b7dc
Ram <parkeryangga@gmail.com>
2024-01-26 11:34:57 +0900

    2차원 배열까지 끝.. 내일 부터 포인터..

---
+++ array2/array2/array2.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int array2[3][2];
+
+	// 가로 길이
+	int array2Col = sizeof(array2[0]) / sizeof(int);
+
+	// 세로 길이 
+	int array2Row = sizeof(array2) / sizeof(array2[0]);
+
+	for (int i = 0; i < array2Row; i++) {
+
+		for (int j = 0; j < array2Col; j++) {
+
+			if (j == 0) {
+				printf("%d번 학생의 국어 점수를 입력 하세요 : ", i+1);
+				scanf_s("%d", &array2[i][j]);
+			}
+			else {
+				printf("%d번 학생의 수학 점수를 입력 하세요 : ", i + 1);
+				scanf_s("%d", &array2[i][j]);
+			}
+
+		}
+		
+	}
+
+	for (int i = 0; i < array2Row; i++) {
+		
+		printf("%d번 학생의 국어 점수 : %d, 수학 점수 %d \n", i + 1, array2[i][0], array2[i][1]);
+
+	}
+
+	return 0;
+}
