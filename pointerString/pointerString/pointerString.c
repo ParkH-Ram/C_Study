e8c3f0a5fb4f1bd9107b931540e12be4bdb0aed6
Ram <parkeryangga@gmail.com>
2024-02-03 20:07:21 +0900

    비쥬얼 스튜디오 업데이트

--- pointerString/pointerString/pointerString.c
+++ pointerString/pointerString/pointerString.c
@@ -1,21 +1,50 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
-	// 문자열 상수
+	//배열을 이용한 문자열 처리 
 
-	// 문자열과 포인터 배열
+	char str[] = "apple"; // 배열을 이용한 처리  -> 복사 
+
+	printf("%s\n", str);
+
+	for (int i = 0; str[i] != '\0'; i++) {  // null 만나기전까지 
+		
+		str[i] -= 32; // 대문자로 변환
+	}
+
+	printf("%s\n", str);
+
+	// 포인터를 이용한 문자열 처리
+
+	char charArray[] = "hello";
+	char* sp = "hello";
+
+	printf("변경 전 문자열 : %s\n", charArray);
+	printf("변경 전 포인터 문자열 : %s\n", sp);
+
+	charArray[1] = 'E';
+	//sp[1] = 'E';  오류 -> 상수는 변경 불가 
 
 
-	char a[5] = "apple";
-	// 문자열의 실제 저장은 apple + null 총 6byte 가 저장된다.
+	printf("변경 후 문자열 : %s\n", charArray);
 
-	printf("%s \n", a);  // %s 의 뜻은 주소부터 null 문자 전까지 
+ 
+	// 주의할점
 
-	printf("%s\n", "apple");
-	printf("%s\n", "apple" + 2);
-	printf("%s\n", a + 2);  // 문자열 시작 부터 2만큼 출력 즉 a[2]인 p 부터 출력 됨
-	printf("%s\n", a + 5); 
+	// 주소 부터 널 문자까지 배열에 복사한다. 
+	char str2[7] = "banana";  // 큰 따옴표를 쓰면 null 문자가 저장되고 실제 저장은 banana\0 이 저장된다.
+	printf("%s\n", str2); // 길이를 6으로 해놓고 출력하면 이상하다...
+
+	// 배열의 이름은 배열의 첫 번째 원소의 주소
+	// buf  == &buf[0];
+	char buf[6];
+	//buf = "apple"; << 오류  주소를 변경해라는 뜻이기 때문에 불가능.
+	// 문자를 복사 하려면 함수가 필요.
+	strcpy(buf, "apple");
+
+	printf("%s", buf);
 
 	return 0;
 }
